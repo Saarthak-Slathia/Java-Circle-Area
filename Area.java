b9acdfbb91c626f8c6278af41cdad3ba8d69b3de
Saarthak Slathia <77338782+Saarthak-Slathia@users.noreply.github.com>
2021-06-15 10:12:18 +0530

    Added Java Files
    Added the Java Files into the repository.

---
+++ Area.java
@@ -0,0 +1,11 @@
+public class Area {
+    public static void main(String[] args) {
+        double pi, r, a;
+
+        r = 10.8; // Radius of the circle
+        pi = 3.14159; // Value of pi, approximately.
+        a = pi * r * r; // Calculating Area
+
+        System.out.println("Area of the circle is " + a);
+    }
+}
